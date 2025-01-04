4985db68a9c5c111fd2e4fd2e59d39cc76dc76e6
Sachin Bangarwa <sachinbangarwa074@gmail.com>
2025-01-03 22:22:33 -0500

    cartModel complete

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,71 @@
+class CartModel {
+  final String productId;
+  final String categoryId;
+  final String productName;
+  final String categoryName;
+  final String salePrice;
+  final String fullPrice;
+  final List productImages;
+  final String deliveryTime;
+  final bool isSale;
+  final String productDescription;
+  final int productQuantity;
+  final double productSubTotal;
+  final dynamic createdAt;
+  final dynamic updatedAt;
+
+  CartModel({
+    required this.productId,
+    required this.categoryId,
+    required this.productName,
+    required this.categoryName,
+    required this.salePrice,
+    required this.fullPrice,
+    required this.productImages,
+    required this.deliveryTime,
+    required this.isSale,
+    required this.productDescription,
+    required this.productQuantity,
+    required this.productSubTotal,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'productId': productId,
+      'categoryId': categoryId,
+      'productName': productName,
+      'categoryName': categoryName,
+      'salePrice': salePrice,
+      'fullPrice': fullPrice,
+      'productImg': productImages,
+      'deliveryTime': deliveryTime,
+      'isSale': isSale,
+      'productDec': productDescription,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'productQuantity': productQuantity,
+      'productSubTotal': productSubTotal
+    };
+  }
+
+  factory CartModel.fromMap(Map<String, dynamic> json) {
+    return CartModel(
+      productId: json['productId'],
+      categoryId: json['categoryId'],
+      productName: json['productName'],
+      categoryName: json['categoryName'],
+      salePrice: json['salePrice'],
+      fullPrice: json['fullPrice'],
+      productImages: json['productImg'],
+      deliveryTime: json['deliveryTime'],
+      isSale: json['isSale'],
+      productDescription: json['productDec'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+      productQuantity: json['productQuantity'],
+      productSubTotal: json['productSubTotal'],
+    );
+  }
+}
